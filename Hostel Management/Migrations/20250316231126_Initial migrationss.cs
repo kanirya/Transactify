cae51758ae21daea8f3f17e5da287a942964caf4
Muhammad <danishkanirya9999@gmail.com>
2025-03-17 19:57:30 +0500

    changing the favicon and other stuff

---
+++ Hostel Management/Migrations/20250316231126_Initial migrationss.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hostel_Management.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initialmigrationss : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
